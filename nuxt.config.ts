2ff25b727bd8b9bc008fef34b1343a23e5fbba7f
sumihiro3 <sumihiro@gmail.com>
2024-02-15 14:20:17 +0900

    HTML タイトルなどを設定

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,26 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   srcDir: './src',
+  app: {
+    head: {
+      title: 'LIFF mock sample',
+      htmlAttrs: {
+        lang: 'ja',
+      },
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        { hid: 'description', name: 'description', content: '' },
+        { name: 'format-detection', content: 'telephone=no' },
+      ],
+      link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    },
+  },
   devtools: { enabled: true },
   runtimeConfig: {
     public: {
       NODE_ENV: process.env.NODE_ENV,
-      LIFF_ID: process.env.LIFF_ID,
+      LIFF_ID: process.env.LIFF_ID || 'TEST-1234567890',
       TEST_USER_ID: process.env.TEST_USER_ID,
     },
   },

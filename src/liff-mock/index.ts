c36ac5a6009eb91f54c4242a3f18379aa43a21ea
sumihiro3 <sumihiro@gmail.com>
2024-02-15 14:20:57 +0900

    e2e テスト用に外部からモックデータを設定できるようにした

--- src/liff-mock/index.ts
+++ src/liff-mock/index.ts
@@ -1,15 +1,20 @@
 import { Profile } from '@liff/get-profile';
-import * as mockData from './mock-data.json';
+import * as mockData from './mock-data.json' assert { type: 'json' };
 
 /**
  * Get mock profile
  * @param testUserId TEST_USER_ID
+ * @param data mock data
  * @returns mock profile
  */
-export const getMockProfile = (testUserId: string): Profile => {
-  const profiles = mockData.profile as Profile[];
+export const getMockProfile = (testUserId: string, data?: any): Profile => {
+  // mock data
+  const profiles = data
+    ? (data.profile as Profile[])
+    : (mockData.profile as Profile[]);
+  // find profile
   let profile = profiles.find((p) => p.userId === testUserId);
-  console.log('profile', profile);
+  // console.log('profile', profile);
   if (!profile) {
     profile = {
       displayName: testUserId,
@@ -31,21 +36,23 @@ interface ScanCodeResult {
 /**
  * Get mock scan code result
  * @param testUserId TEST_USER_ID
+ * @param data mock data
  * @returns mock scan code result
  */
-export const getMockScanCodeResult = (testUserId: string): ScanCodeResult => {
-  const scanCodeResults = mockData.scanCode as MockScanCodeResult[];
+export const getMockScanCodeResult = (
+  testUserId: string,
+  data?: any,
+): ScanCodeResult => {
+  // mock data
+  const scanCodeResults = data
+    ? (data.scanCode as MockScanCodeResult[])
+    : (mockData.scanCode as MockScanCodeResult[]);
+  // find scan code result
   const scanCodeResult = scanCodeResults.find((s) => s.userId === testUserId);
-  console.log('scanCodeResult', scanCodeResult);
-  let result;
+  // console.log('scanCodeResult', scanCodeResult);
+  let result: ScanCodeResult = { value: null };
   if (scanCodeResult) {
-    result = {
-      value: scanCodeResult.value,
-    };
-  } else {
-    result = {
-      value: null,
-    };
+    result.value = scanCodeResult.value;
   }
   return result;
 };
